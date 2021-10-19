d03504e60ab9a091461d83d37c4704347ae37130
IndiraHuseinagic <huseinagic.indira@gmail.com>
2021-10-19 09:36:10 +0200

    Mean stack app

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/modern-home'
+   //db: 'mongodb://localhost:27017/modern-home'
+   db: 'mongodb+srv://admindb:njemacki@cluster0.hvy54.mongodb.net/test'
 };
